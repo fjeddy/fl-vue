4c8bcdc5637479743b90aa71a50c8eece7334815
Freddy Lundekvam <freddy@lundekvam.info>
2021-01-21 04:13:03 +0100

    Add dismissible option

--- src/components/FlAlert/FlAlert.js
+++ src/components/FlAlert/FlAlert.js
@@ -1,23 +1,54 @@
 export default {
   name: 'FlAlert',
 
+  data: function() {
+    return {
+      visible: true
+    }
+  },
+
   props: {
     type: {
       type: String,
       default: 'info'
-    }
+    },
+
+    dismissible: Boolean
   },
 
   render: function(h) {
+    if (!this.visible) return
+
+    let self = this
+    let elements = [
+      this.$slots.default
+    ]
+
+    if (this.dismissible) {
+      elements.push(h(
+        'button',
+        {
+          class: 'btn-close',
+          on: {
+            click: function(event) {
+              event.preventDefault()
+              self.visible = false
+            }
+          }
+        }
+      ))
+    }
+
     return h(
       'div',
       {
         class: {
           'alert': true,
-          [`alert-${this.type}`]: true
+          [`alert-${this.type}`]: true,
+          'alert-dismissible fade': this.dismissible
         }
       },
-      this.$slots.default
+      elements
     )
   }
 }
